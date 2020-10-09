9ca902246c56438e9e954be754e48d2a938a0fc5
hopshackle <james@janigo.co.uk>
2020-10-09 18:16:09 +0100

    Merged in Gaussian code

--- src/mathFunctions/FunctionReport.kt
+++ src/mathFunctions/FunctionReport.kt
@@ -9,9 +9,31 @@ import kotlin.math.*
 
 
 fun main(args: Array<String>) {
+    if (args.size < 4 || args.contains("-h")) {
+        println(
+            """
+                Core required command line arguments in order:
+                - Type of Function: Hartmann3|Hartmann6|Branin|GoldsteinPrice
+                - Type of NTBEA function: STD|FIT|STDFIT|EXP|LIN|INV|SQRT|GAUSSIAN  (STD works best almost always)
+                - Number of NTBEA runs, and trials for each in format "runs|trials"
+                - Discretization level to use for each parameter dimension
+                
+                Additional optional arguments:
+                kExplore=                   The constant to use in the UCB formula. Large k explores more. Default is 100.
+                minWeight=                  Only used for EXP, LIN, SQRT or INV weight functions. Frankly I wouldn't bother.
+                fitWeight=                  Weight to apply to the Linear Regression function when using STDFIT - between 0.0 and 1.0. Default is 0.5.
+                maxF=                       Maximum number of features to use for FIT or STDFIT in the regression component. Default is 0 (meaning no maximum).
+                T=                          For EXP/LIN/SQRT/INV, this is the temperature to use in the weight function. Default is 30.
+                T=                          For FIT/STDFIT this is instead the threshold number of visits before a Tuple is incorporated in the regression. Default is 30.
+                hood=                       The number of neighbouring points to test at each iteration of NTBEA before picking one to sample with the function.
+                logFile=                    A filename to write the results too (one line per NTBEA run).
+            """.trimIndent()
+        )
+    }
+
     val f = when (args[0]) {
-//        "Hartmann3" -> Hartmann3
-//        "Hartmann6" -> Hartmann6
+        "Hartmann3" -> Hartmann3
+        "Hartmann6" -> Hartmann6
         "Branin" -> Branin
         "GoldsteinPrice" -> GoldsteinPrice
         else -> throw AssertionError("Invalid first argument for function " + args[0])
@@ -19,7 +41,13 @@ fun main(args: Array<String>) {
     val type = args[1]
     val iterations = args[2].split("|")
     val dimensions = args[3].toInt()
-    FunctionReport(f).iterations(type, iterations[0].toInt(), iterations[1].toInt(), Pair(f.dimension, dimensions), args)
+    FunctionReport(f).iterations(
+        type.toUpperCase(),
+        iterations[0].toInt(),
+        iterations[1].toInt(),
+        Pair(f.dimension, dimensions),
+        args
+    )
 }
 
 class FunctionReport(val f: NTBEAFunction) {
@@ -28,29 +56,25 @@ class FunctionReport(val f: NTBEAFunction) {
         // for each run we keep the final best sampled point...and its actual value under the function
         // We can then report the basic stats for imprecision and also actual value
 
-        val searchSpaceType = args.find { it.startsWith("space") }?.split("=")?.get(1)?: "normal"
-        var searchSpace: SearchSpace
-
-        if(searchSpaceType == "gaussian") {
-            searchSpace = GaussianSearchSpace(
+        val searchSpace: SearchSpace = if (type == "GAUSSIAN") {
+            GaussianSearchSpace(
                 searchDimensions.first,
-                IntArray(searchDimensions.first*2) {searchDimensions.second},
+                IntArray(searchDimensions.first * 2) { searchDimensions.second },
                 doubleArrayOf(0.0, 0.0),
                 doubleArrayOf(1.0, 1.0),
                 doubleArrayOf(0.1, 0.1),
                 doubleArrayOf(0.2, 0.2)
             )
         } else {
-            searchSpace = FunctionSearchSpace(searchDimensions.first, searchDimensions.second)
+            FunctionSearchSpace(searchDimensions.first, searchDimensions.second)
         }
         val kExplore = (args.find { it.startsWith("kExplore=") }?.split("=")?.get(1) ?: "100.0").toDouble()
         val minWeight = (args.find { it.startsWith("minWeight=") }?.split("=")?.get(1) ?: "0.0").toDouble()
         val weight = (args.find { it.startsWith("fitWeight=") }?.split("=")?.get(1) ?: "0.5").toDouble()
         val maxFeatures = (args.find { it.startsWith("maxF=") }?.split("=")?.get(1) ?: "0").toInt()
         val T = (args.find { it.startsWith("T=") }?.split("=")?.get(1) ?: "30").toInt()
-        val neighbourhood = 50;
-//        val neighbourhood: Double = args.find { it.startsWith("hood=") }?.split("=")?.get(1)?.toDouble()
-//                ?: min(50.0, searchDimensions.second.toDouble().pow(searchDimensions.first) * 0.01)
+        val neighbourhood: Double = args.find { it.startsWith("hood=") }?.split("=")?.get(1)?.toDouble()
+            ?: min(50.0, searchDimensions.second.toDouble().pow(searchDimensions.first) * 0.01)
         val fileName: String = args.find { it.startsWith("logFile=") }?.split("=")?.get(1) ?: ""
 
         val weightFunction: (Int) -> Double = when (type) {
@@ -63,10 +87,28 @@ class FunctionReport(val f: NTBEAFunction) {
         }
 
         val landscapeModel: LandscapeModel = when (type) {
-            "STD" -> NTupleSystem(searchSpace)
-            "FIT" -> NTupleSystemBinaryFit(searchSpace, threshold = T, interpolateByTuple = true, maxFeatures = maxFeatures)
-            "STDFIT" -> NTupleSystemBinaryFit(searchSpace, threshold = T, interpolateByTuple = false, interpolation = weight, maxFeatures = maxFeatures)
-            else -> NTupleSystemExp(searchSpace, T, minWeight, weightFunction, weightExplore = false, exploreWithSqrt = false)
+            "STD", "GAUSSIAN" -> NTupleSystem(searchSpace)
+            "FIT" -> NTupleSystemBinaryFit(
+                searchSpace,
+                threshold = T,
+                interpolateByTuple = true,
+                maxFeatures = maxFeatures
+            )
+            "STDFIT" -> NTupleSystemBinaryFit(
+                searchSpace,
+                threshold = T,
+                interpolateByTuple = false,
+                interpolation = weight,
+                maxFeatures = maxFeatures
+            )
+            else -> NTupleSystemExp(
+                searchSpace,
+                T,
+                minWeight,
+                weightFunction,
+                weightExplore = false,
+                exploreWithSqrt = false
+            )
         }
 
         val use3Tuples = args.contains("useThreeTuples")
@@ -102,8 +144,11 @@ class FunctionReport(val f: NTBEAFunction) {
             actualValues[choiceVals.joinToString(",")] = actualValue
             GlobalStatsCollator.addStatistics("ActualValue", actualValue)
             GlobalStatsCollator.addStatistics("Delta", predictedValue - actualValue)
-            val details = String.format("%.3g, %.3g, %.3g, %s\n", predictedValue, actualValue, predictedValue - actualValue,
-                    landscapeModel.bestOfSampled.joinToString(separator = "|", transform = { it.toInt().toString() }))
+            val details =
+                String.format(
+                    "%.3g, %.3g, %.3g, %s\n", predictedValue, actualValue, predictedValue - actualValue,
+                    landscapeModel.bestOfSampled.joinToString(separator = "|", transform = { it.toInt().toString() })
+                )
             if (fileWriter != null)
                 fileWriter.write(details)
             else
